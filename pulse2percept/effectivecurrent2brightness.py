add 1.0 Nanduri output scaling factor; fix 1.2 analysis

pulse2percept/effectivecurrent2brightness.py
@@ -17,6 +17,7 @@ from pulse2percept import utils
 
 
 class TemporalModel(object):
+
     def __init__(self, model='Nanduri', tsample=5e-6, tau1=4.2e-4,
                  tau2=4.525e-2, tau3=2.625e-2, epsilon=8.73, asymptote=14,
                  slope=3, shift=16):
@@ -83,6 +84,12 @@ class TemporalModel(object):
         self.slope = slope
         self.shift = shift
 
+        # Scaling factor needed to convert output to sensical brightness values
+        if model == 'Horsager':
+            self.scale_output = 1.0
+        else:
+            self.scale_output = 137.4
+
         # perform onte-time setup calculations
         # Gamma functions used as convolution kernels do not depend on input
         # data, hence can be calculated once, then re-used (trade off memory
@@ -98,7 +105,6 @@ class TemporalModel(object):
         # gamma3 is used to calculate the slow response
         t = np.arange(0, 8 * self.tau3, self.tsample)
         self.gamma3 = e2cm.gamma(3, self.tau3, t)
-
 
     def fast_response(self, b1, dojit=True):
         """Fast response function (Box 2)
@@ -133,7 +139,6 @@ class TemporalModel(object):
         # the dimensions of the input signal.
         return self.tsample * conv[:b1.shape[-1]]
 
-
     def charge_accumulation(self, b2):
         """Charge accumulation step (Box 3)
 
@@ -163,7 +168,6 @@ class TemporalModel(object):
         # Cut off the tail of the convolution to make the output signal match
         # the dimensions of the input signal
         return self.epsilon * self.tsample * conv[:b2.shape[-1]]
-
 
     def stationary_nonlinearity(self, b3):
         """Stationary nonlinearity (Box 4)
@@ -302,7 +306,7 @@ class TemporalModel(object):
         ca = self.charge_accumulation(ecm.data)
         resp = self.stationary_nonlinearity(resp - ca)
         resp = self.slow_response(resp)
-        return utils.TimeSeries(self.tsample, resp)
+        return utils.TimeSeries(self.tsample, resp * self.scale_output)
 
     def cascade_krishnan(self, ecm, dojit):
         """Model cascade according to Krishnan et al. (2015).
@@ -326,7 +330,7 @@ class TemporalModel(object):
         resp = self.fast_response(ecm.data - ca, dojit=dojit)
         resp = self.stationary_nonlinearity(resp)
         resp = self.slow_response(resp)
-        return utils.TimeSeries(self.tsample, resp)
+        return utils.TimeSeries(self.tsample, resp * self.scale_output)
 
     def cascade_horsager(self, ecm, dojit):
         """Model cascade according to Horsager et al. (2009).
@@ -339,7 +343,7 @@ class TemporalModel(object):
         resp = self.fast_response(ecm.data - ca, dojit=dojit)
         resp = self.power_nonlinearity(resp)
         resp = self.slow_response(resp)
-        return utils.TimeSeries(self.tsample, resp)
+        return utils.TimeSeries(self.tsample, resp * self.scale_output)
 
 
 def pulse2percept(temporal_model, ecs, retina, stimuli, rs, engine='joblib',
@@ -387,7 +391,8 @@ def calc_pixel(ecs_vector, stim_data, temporal_model, resample_factor,
     sr.resample(resample_factor)
     return sr
 
-def onoffFiltering(movie, n, sig=[.1, .25],amp=[.01, -0.005]):
+
+def onoffFiltering(movie, n, sig=[.1, .25], amp=[.01, -0.005]):
     """
     From a movie to a version that is filtered by a collection on and off cells
     of sizes
@@ -399,75 +404,76 @@ def onoffFiltering(movie, n, sig=[.1, .25],amp=[.01, -0.005]):
     """
     onmovie = np.zeros([movie.data.shape[0], movie.data.shape[1], movie.data.shape[2]])
     offmovie = np.zeros([movie.data.shape[0], movie.data.shape[1], movie.data.shape[2]])
-    newfiltImgOn=np.zeros([movie.shape[0], movie.shape[1]])
-    newfiltImgOff=np.zeros([movie.shape[0], movie.shape[1]])
-    pad = max(n)*2
+    newfiltImgOn = np.zeros([movie.shape[0], movie.shape[1]])
+    newfiltImgOff = np.zeros([movie.shape[0], movie.shape[1]])
+    pad = max(n) * 2
     for xx in range(movie.shape[-1]):
-        oldimg=movie[:, :, xx].data
-        tmpimg=np.mean(np.mean(oldimg))*np.ones([oldimg.shape[0]+pad*2,oldimg.shape[1]+pad*2])
+        oldimg = movie[:, :, xx].data
+        tmpimg = np.mean(np.mean(oldimg)) * np.ones([oldimg.shape[0] + pad * 2, oldimg.shape[1] + pad * 2])
         img = insertImg(tmpimg, oldimg)
-        filtImgOn=np.zeros([img.shape[0], img.shape[1]])
-        filtImgOff=np.zeros([img.shape[0], img.shape[1]])
-        
-        for i in range(n.shape[0]): 
-            [x,y] = np.meshgrid(np.linspace(-1,1,n[i]),np.linspace(-1,1,n[i]))   
-            rsq = x**2+y**2
-            dx = x[0,1]-x[0,0]    
-            on = np.exp(-rsq/(2*sig[0]**2))*(dx**2)/(2*np.pi*sig[0]**2)
-            off = np.exp(-rsq/(2*sig[1]**2))*(dx**2)/(2*np.pi*sig[1]**2)
-            filt = on-off
-            tmp_on = convolve2d(img,filt,'same')/n.shape[-1]
-            tmp_off=tmp_on
-            tmp_on= np.where(tmp_on>0, tmp_on, 0) 
-            tmp_off= -np.where(tmp_off<0, tmp_off, 0)
-             #   rectified = np.where(ptrain.data > 0, ptrain.data, 0)
-            filtImgOn =    filtImgOn+tmp_on/n.shape[0] 
-            filtImgOff =   filtImgOff+tmp_off/n.shape[0] 
+        filtImgOn = np.zeros([img.shape[0], img.shape[1]])
+        filtImgOff = np.zeros([img.shape[0], img.shape[1]])
+
+        for i in range(n.shape[0]):
+            [x, y] = np.meshgrid(np.linspace(-1, 1, n[i]), np.linspace(-1, 1, n[i]))
+            rsq = x**2 + y**2
+            dx = x[0, 1] - x[0, 0]
+            on = np.exp(-rsq / (2 * sig[0]**2)) * (dx**2) / (2 * np.pi * sig[0]**2)
+            off = np.exp(-rsq / (2 * sig[1]**2)) * (dx**2) / (2 * np.pi * sig[1]**2)
+            filt = on - off
+            tmp_on = convolve2d(img, filt, 'same') / n.shape[-1]
+            tmp_off = tmp_on
+            tmp_on = np.where(tmp_on > 0, tmp_on, 0)
+            tmp_off = -np.where(tmp_off < 0, tmp_off, 0)
+            #   rectified = np.where(ptrain.data > 0, ptrain.data, 0)
+            filtImgOn = filtImgOn + tmp_on / n.shape[0]
+            filtImgOff = filtImgOff + tmp_off / n.shape[0]
 
         # Remove padding
-        nopad=np.zeros([img.shape[0]-pad*2, img.shape[1]-pad*2])
-        newfiltImgOn[:,:] = insertImg(nopad,filtImgOn)
-        newfiltImgOff[:, :] = insertImg(nopad,filtImgOff)
-        onmovie[:, :, xx]=newfiltImgOn
-        offmovie[:, :, xx]=newfiltImgOff
-        
+        nopad = np.zeros([img.shape[0] - pad * 2, img.shape[1] - pad * 2])
+        newfiltImgOn[:, :] = insertImg(nopad, filtImgOn)
+        newfiltImgOff[:, :] = insertImg(nopad, filtImgOff)
+        onmovie[:, :, xx] = newfiltImgOn
+        offmovie[:, :, xx] = newfiltImgOff
+
     return (onmovie, offmovie)
+
 
 def onoffRecombine(onmovie, offmovie):
     """
-    From a movie as filtered by on and off cells, 
-    to a recombined version that is either based on an electronic 
+    From a movie as filtered by on and off cells,
+    to a recombined version that is either based on an electronic
     prosthetic (on + off) or recombined as might be done by a cortical
-    cell in normal vision (on-off) 
+    cell in normal vision (on-off)
     Parameters
     ----------
     movie: on and off movies to be recombined
     combination : options are 'both' returns both prosthetic and normal vision, 'normal' and 'prosthetic'
-    """  
+    """
 
-    prostheticmovie=onmovie + offmovie
-    normalmovie=onmovie - offmovie
+    prostheticmovie = onmovie + offmovie
+    normalmovie = onmovie - offmovie
     return (normalmovie, prostheticmovie)
 
 
-def insertImg(out_img,in_img): 
+def insertImg(out_img, in_img):
     """ insertImg(out_img,in_img)
-    Inserts in_img into the center of out_img.  
+    Inserts in_img into the center of out_img.
     if in_img is larger than out_img, in_img is cropped and centered.
     """
 
-    if in_img.shape[0]>out_img.shape[0]:
-        x0 = np.floor([(in_img.shape[0]-out_img.shape[0])/2])
-        xend=x0+out_img.shape[0]    
-        in_img=in_img[x0:xend, :]
-       
-    if in_img.shape[1]>out_img.shape[1]:
-        y0 = np.floor([(in_img.shape[1]-out_img.shape[1])/2])   
-        yend=y0+out_img.shape[1]
-        in_img=in_img[:, y0:yend]
-       
-    x0 = np.floor([(out_img.shape[0]-in_img.shape[0])/2])
-    y0 = np.floor([(out_img.shape[1]-in_img.shape[1])/2])
-    out_img[x0:x0+in_img.shape[0], y0:y0+in_img.shape[1]] = in_img
-    
+    if in_img.shape[0] > out_img.shape[0]:
+        x0 = np.floor([(in_img.shape[0] - out_img.shape[0]) / 2])
+        xend = x0 + out_img.shape[0]
+        in_img = in_img[x0:xend, :]
+
+    if in_img.shape[1] > out_img.shape[1]:
+        y0 = np.floor([(in_img.shape[1] - out_img.shape[1]) / 2])
+        yend = y0 + out_img.shape[1]
+        in_img = in_img[:, y0:yend]
+
+    x0 = np.floor([(out_img.shape[0] - in_img.shape[0]) / 2])
+    y0 = np.floor([(out_img.shape[1] - in_img.shape[1]) / 2])
+    out_img[x0:x0 + in_img.shape[0], y0:y0 + in_img.shape[1]] = in_img
+
     return out_img

pulse2percept/tests/test_effectivecurrent2brightness.py
@@ -5,6 +5,54 @@ import pulse2percept.electrode2currentmap as e2cm
 import pulse2percept.effectivecurrent2brightness as ec2b
 
 
+def test_nanduri_amp_freq():
+    # Make sure the Nanduri version of the model can reproduce amp/freq
+    # data from Nanduri et al. (2012)
+    tsample = 5e-6
+    tm = ec2b.TemporalModel(model='Nanduri', tsample=tsample)
+
+    # input amplitude values and expected output
+    exp_thresh = 30
+    all_amps = np.array([1.25, 1.5, 2, 4, 6]) * exp_thresh
+    out_amps = np.array([10, 15, 18, 19, 19])
+
+    # input frequency values and expected output
+    all_freqs = np.array([13, 20, 27, 40, 80, 120])
+    out_freqs = np.array([7.3, 10, 13, 19, 34, 51])
+
+    ## --- Step 1: Reproduce brightness vs. amplitude ---------------------- ##
+    # Run the model on these values and compare model output vs. Nanduri data.
+    amp_nanduri = []
+    for ampl in all_amps:
+        pt = e2cm.Psycho2Pulsetrain(freq=all_freqs[1], dur=0.5,
+                                    pulse_dur=0.45 / 1000,
+                                    interphase_dur=0, delay=0,
+                                    tsample=tsample,
+                                    current_amplitude=ampl,
+                                    pulsetype='cathodicfirst')
+        R4 = tm.model_cascade(pt, dojit=False)
+        amp_nanduri.append(R4.data.max())
+    amp_nanduri = np.array(amp_nanduri)
+
+    npt.assert_allclose(amp_nanduri, out_amps, rtol=1)
+
+    ## --- Step 2: Reproduce brightness vs. frequency ---------------------- ##
+    # Run the model on these values and compare model output vs. Nanduri data.
+    freq_nanduri = []
+    for freq in all_freqs:
+        pt = e2cm.Psycho2Pulsetrain(freq=freq, dur=0.5,
+                                    pulse_dur=0.45 / 1000,
+                                    interphase_dur=0, delay=0,
+                                    tsample=tsample,
+                                    current_amplitude=all_amps[0],
+                                    pulsetype='cathodicfirst')
+        R4 = tm.model_cascade(pt, dojit=False)
+        freq_nanduri.append(R4.data.max())
+    freq_nanduri = np.array(freq_nanduri)
+
+    npt.assert_allclose(freq_nanduri, out_freqs, rtol=1)
+
+
 def test_nanduri_vs_krishnan():
     """Test Nanduri vs Krishnan model
 
@@ -17,7 +65,7 @@ def test_nanduri_vs_krishnan():
     tau2 = 0.04525
     tau3 = 0.02625
     epsilon = 8.73
-    tol = 0.01
+    tol = 3
 
     # Set up both models with the same parameter values
     tm_nanduri = ec2b.TemporalModel(model='Nanduri', tsample=tsample,
@@ -31,7 +79,8 @@ def test_nanduri_vs_krishnan():
     for freq in [5, 10, 20]:
         for ampl in [10, 30, 50]:
             # Define some arbitrary pulse train
-            pulse = e2cm.Psycho2Pulsetrain(freq=freq, dur=0.5, pulse_dur=4.5e-4,
+            pulse = e2cm.Psycho2Pulsetrain(freq=freq, dur=0.5,
+                                           pulse_dur=4.5e-4,
                                            interphase_dur=4.5e-4, delay=0,
                                            tsample=tsample,
                                            current_amplitude=ampl,
@@ -43,7 +92,7 @@ def test_nanduri_vs_krishnan():
 
             # Make sure model output doesn't deviate too much
             npt.assert_allclose(np.sum((out_nanduri.data -
-                                       out_krishnan.data)**2), 0, atol=tol)
+                                        out_krishnan.data) ** 2), 0, atol=tol)
 
 
 def test_brightness_movie():
@@ -56,9 +105,9 @@ def test_brightness_movie():
     retina = e2cm.Retina(xlo=xlo, xhi=xhi, ylo=ylo, yhi=yhi,
                          sampling=sampling, axon_map=retina_file)
 
-    s1 = e2cm.Psycho2Pulsetrain(freq=20, dur=0.5, pulse_dur=.075/1000.,
-                                interphase_dur=.075/1000., delay=0.,
-                                tsample=.075/1000., current_amplitude=20,
+    s1 = e2cm.Psycho2Pulsetrain(freq=20, dur=0.5, pulse_dur=.075 / 1000.,
+                                interphase_dur=.075 / 1000., delay=0.,
+                                tsample=.075 / 1000., current_amplitude=20,
                                 pulsetype='cathodicfirst')
 
     electrode_array = e2cm.ElectrodeArray([1, 1], [0, 1], [0, 1], [0, 1])
@@ -75,9 +124,9 @@ def test_brightness_movie():
     amplitude_transform = 'linear'
     amp_max = 90
     freq = 20
-    pulse_dur = .075/1000.
-    interphase_dur = .075/1000.
-    tsample = .005/1000.
+    pulse_dur = .075 / 1000.
+    interphase_dur = .075 / 1000.
+    tsample = .005 / 1000.
     pulsetype = 'cathodicfirst'
     stimtype = 'pulsetrain'
     dtype = np.int8