54b2584e42762a0904303d349d77c47861bebee0
Michael Beyeler <mbeyeler@uw.edu>
2019-09-09 16:03:58 -0700

    remove io tests

--- pulse2percept/setup.py
+++ pulse2percept/setup.py
@@ -22,7 +22,6 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('utils')
     config.add_subpackage('utils/tests')
     config.add_subpackage('io')
-    config.add_subpackage('io/tests')
 
     # Submodules which have their own setup.py; e.g., because they use Cython:
     config.add_subpackage('models')
